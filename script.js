0879f7209a59504711a3ac6b2db6efa5c37a5347
Kevin <93732218+KGBorges@users.noreply.github.com>
2023-10-19 21:58:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ function contar() {
     let passo = document.getElementById('txtp')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
         if (i < f) {
             //contagem crescente
             for(let c = i; c <= f; c += p) {
